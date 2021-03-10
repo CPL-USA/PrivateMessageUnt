b46a66609a956161006aed871acdf5069ac231dc
CPL USA <CPL USA@DESKTOP-4D9RMEI>
2021-03-10 18:36:09 +0200

    Add project files.

---
+++ PrivateMessageUnt/Commands/CommandPM.cs
@@ -0,0 +1,49 @@
+﻿using Rocket.API;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace PrivateMessageUnt.Commands
+{
+    class CommandPM : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Player;
+
+        public string Name => "pm";
+
+        public string Help => string.Empty;
+
+        public string Syntax => string.Empty;
+
+        public List<string> Aliases => new List<string>();
+
+        public List<string> Permissions => new List<string>();
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            UnturnedPlayer player = (UnturnedPlayer)caller;
+
+            if (command.Length<=1)
+            {
+                UnturnedChat.Say(player, PrivateMessage.Instance.Translate("command_pm_error"), Color.red);
+            }
+            else
+            {
+                UnturnedPlayer player2 = UnturnedPlayer.FromName(command[0]);
+                if (player2 == null)
+                {
+                    UnturnedChat.Say(player, PrivateMessage.Instance.Translate("command_pm_player_not_found", command[0]), Color.red);
+                    return;
+                }
+                UnturnedChat.Say(player2, PrivateMessage.Instance.Translate("command_pm_delivered", player.CharacterName, command[1]), Color.magenta);
+            }
+
+
+        }
+    }
+}
---
+++ PrivateMessageUnt/PrivateMessage.cs
@@ -0,0 +1,40 @@
+﻿using Rocket.API.Collections;
+using Rocket.Core.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrivateMessageUnt
+{
+    public class PrivateMessage : RocketPlugin
+    {
+        public static PrivateMessage Instance;
+
+
+        protected override void Load()
+        {
+            Instance = this;
+        }
+
+        protected override void Unload()
+        {
+
+        }
+
+
+
+        public override TranslationList DefaultTranslations => new TranslationList()
+        {
+            {"command_pm_error", "/pm [nickname] [message]"},
+            {"command_pm_delivered", "{0} пишет: {1}"},
+            {"command_pm_player_not_found", "{0} не найден." },
+        };
+
+
+
+
+
+    }
+}
